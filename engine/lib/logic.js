e759816386e2f0a97a4dc66793958e4cc4fa21b7
kayra-ea <kayra@kayra.ca>
2021-08-09 18:19:17 -0400

    Pawns can now move correctly, but not capture

--- engine/lib/logic.js
+++ engine/lib/logic.js
@@ -20,7 +20,7 @@ exports.movePiece = function movePiece(
   squares,
   selectedSquare,
   prevSelectedSquare,
-  reqStatus,
+  reqStatus
 ) {
   let cpy_squares = squares.slice();
   let cpy_selectedSquare = Object.assign({}, selectedSquare);
@@ -50,6 +50,7 @@ exports.movePiece = function movePiece(
     }
   } catch (error) {
     reqStatus = "FAILED";
+    console.error(error);
     console.error(`Error - Could not move Piece`);
   }
 
@@ -69,16 +70,34 @@ function isLegalMove(squares, selectedSquare, prevSelectedSquare) {
   const piece = squares[prevSelectedSquare.x][prevSelectedSquare.y];
   let isLegalMove = false;
   let moves = [];
+  let pawnFirstMove;
   debugger;
 
   switch (piece) {
     case cnst.WHITE_PAWN:
-      moves = getPawnMoves(prevSelectedSquare.x, prevSelectedSquare.y,cnst.WHITE_PAWN,true); //hardcoded to be every turn as first turn
+      pawnFirstMove = isFirstPawnMove(cnst.WHITE_PAWN, prevSelectedSquare.x);
+      console.log("Getting WHITE pawn moves!!!!");
+      //console.log("Pawn First move: ", isFirstPawnMove);
+      moves = getPawnMoves(
+        prevSelectedSquare.x,
+        prevSelectedSquare.y,
+        cnst.WHITE_PAWN,
+        pawnFirstMove
+      );
+      console.log(`The moves the WHITE pawn can make are: `, moves);
       isLegalMove = checkIfValid(moves, selectedSquare.x, selectedSquare.y);
       break;
 
     case cnst.BLACK_PAWN:
-      moves = getPawnMoves(prevSelectedSquare.x, prevSelectedSquare.y,cnst.BLACK_PAWN,true); //hardcoded to be every turn as first turn
+      pawnFirstMove = isFirstPawnMove(cnst.BLACK_PAWN, prevSelectedSquare.x);
+      moves = getPawnMoves(
+        prevSelectedSquare.x,
+        prevSelectedSquare.y,
+        cnst.BLACK_PAWN,
+        pawnFirstMove
+      );
+      console.log("Getting BLACK pawn moves!!!!");
+      console.log(`The moves the BLACK pawn can make are: `, moves);
       isLegalMove = checkIfValid(moves, selectedSquare.x, selectedSquare.y);
       break;
 
@@ -92,39 +111,79 @@ function isLegalMove(squares, selectedSquare, prevSelectedSquare) {
       break;
 
     case cnst.WHITE_BISHOP:
-      moves = getBishopMoves(prevSelectedSquare.x, prevSelectedSquare.y, squares, cnst.WHITE_BISHOP);
+      moves = getBishopMoves(
+        prevSelectedSquare.x,
+        prevSelectedSquare.y,
+        squares,
+        cnst.WHITE_BISHOP
+      );
       isLegalMove = checkIfValid(moves, selectedSquare.x, selectedSquare.y);
       break;
 
     case cnst.BLACK_BISHOP:
-      moves = getBishopMoves(prevSelectedSquare.x, prevSelectedSquare.y, squares, cnst.BLACK_BISHOP);
+      moves = getBishopMoves(
+        prevSelectedSquare.x,
+        prevSelectedSquare.y,
+        squares,
+        cnst.BLACK_BISHOP
+      );
       isLegalMove = checkIfValid(moves, selectedSquare.x, selectedSquare.y);
       break;
 
     case cnst.WHITE_ROOK:
-      moves = getRookMoves(prevSelectedSquare.x, prevSelectedSquare.y, squares, cnst.WHITE_ROOK);
+      moves = getRookMoves(
+        prevSelectedSquare.x,
+        prevSelectedSquare.y,
+        squares,
+        cnst.WHITE_ROOK
+      );
       isLegalMove = checkIfValid(moves, selectedSquare.x, selectedSquare.y);
       break;
     case cnst.BLACK_ROOK:
-      moves = getRookMoves(prevSelectedSquare.x, prevSelectedSquare.y, squares, cnst.BLACK_ROOK);
+      moves = getRookMoves(
+        prevSelectedSquare.x,
+        prevSelectedSquare.y,
+        squares,
+        cnst.BLACK_ROOK
+      );
       isLegalMove = checkIfValid(moves, selectedSquare.x, selectedSquare.y);
       break;
 
     case cnst.WHITE_QUEEN:
-      moves = getQueenMoves(prevSelectedSquare.x, prevSelectedSquare.y, squares, cnst.WHITE_QUEEN);
+      moves = getQueenMoves(
+        prevSelectedSquare.x,
+        prevSelectedSquare.y,
+        squares,
+        cnst.WHITE_QUEEN
+      );
       isLegalMove = checkIfValid(moves, selectedSquare.x, selectedSquare.y);
       break;
     case cnst.BLACK_QUEEN:
-      moves = getQueenMoves(prevSelectedSquare.x, prevSelectedSquare.y, squares, cnst.BLACK_QUEEN);
+      moves = getQueenMoves(
+        prevSelectedSquare.x,
+        prevSelectedSquare.y,
+        squares,
+        cnst.BLACK_QUEEN
+      );
       isLegalMove = checkIfValid(moves, selectedSquare.x, selectedSquare.y);
       break;
 
     case cnst.WHITE_KING:
-      moves = getKingMoves(prevSelectedSquare.x, prevSelectedSquare.y, squares, cnst.WHITE_KING);
+      moves = getKingMoves(
+        prevSelectedSquare.x,
+        prevSelectedSquare.y,
+        squares,
+        cnst.WHITE_KING
+      );
       isLegalMove = checkIfValid(moves, selectedSquare.x, selectedSquare.y);
       break;
     case cnst.BLACK_KING:
-      moves = getKingMoves(prevSelectedSquare.x, prevSelectedSquare.y, squares, cnst.BLACK_KING);
+      moves = getKingMoves(
+        prevSelectedSquare.x,
+        prevSelectedSquare.y,
+        squares,
+        cnst.BLACK_KING
+      );
       isLegalMove = checkIfValid(moves, selectedSquare.x, selectedSquare.y);
       break;
 
@@ -136,35 +195,33 @@ function isLegalMove(squares, selectedSquare, prevSelectedSquare) {
   return isLegalMove;
 }
 
-
 /**
  * This fucntion returns as a string the colour of the piece.
  *
  * @param {Object} piece - the unicode string of the pioce
  * @returns {Boolean} getPieceTeam: returns white if the piece's string ends with a number, and black if a letter.
  */
-function getPieceTeam(piece){
+function getPieceTeam(piece) {
   let team;
   let pieceTypeCode;
   let uniChar;
 
-     //for whatever reason, some chars come back as NAN without the leading h
-     piece = 'h' + piece;
-     uniChar = piece.charCodeAt(1).toString(16);
-     uniChar = uniChar.toUpperCase();
-     uniChar = "\\u0" + uniChar;
-     piece = piece.charAt(0) + uniChar;
-     pieceTypeCode = piece.charAt(7);
-   
-     //white pieces end with a number, black pieces end with a letter
-     if (pieceTypeCode >= '0' && pieceTypeCode <= '9'){
-       team = 'WHITE'
-     }
-     else{
-         team = 'BLACK'
-     }
-  
-     return team;
+  //for whatever reason, some chars come back as NAN without the leading h
+  piece = "h" + piece;
+  uniChar = piece.charCodeAt(1).toString(16);
+  uniChar = uniChar.toUpperCase();
+  uniChar = "\\u0" + uniChar;
+  piece = piece.charAt(0) + uniChar;
+  pieceTypeCode = piece.charAt(7);
+
+  //white pieces end with a number, black pieces end with a letter
+  if (pieceTypeCode >= "0" && pieceTypeCode <= "9") {
+    team = "WHITE";
+  } else {
+    team = "BLACK";
+  }
+
+  return team;
 }
 
 /**
@@ -173,20 +230,20 @@ function getPieceTeam(piece){
  * @param {Object} squares - The array of locations of pieces on the board
  * @returns {Boolean} isBlocking: returns true if the desired move is blocked by a piece on the selected square.
  */
-function isBlocking(i, y, squares){
-  if (squares[i][y] !== ""){ return true; }
-  else return false;
+function isBlocking(i, y, squares) {
+  if (squares[i][y] !== "") {
+    return true;
+  } else return false;
 }
 
-function isBlockingOpp(i, y, squares, pieceType){
+function isBlockingOpp(i, y, squares, pieceType) {
   let playerTeam = getPieceTeam(pieceType);
-  let selectedTeam = getPieceTeam(squares[i][y])
+  let selectedTeam = getPieceTeam(squares[i][y]);
 
-  if (playerTeam !== selectedTeam){
+  if (playerTeam !== selectedTeam) {
     return true;
-  }
-  else{
-    return false;;
+  } else {
+    return false;
   }
 }
 
@@ -227,7 +284,7 @@ function getKnightMoves(i, y) {
 function getKingMoves(i, y, squares, pieceType, hasNotCastled) {
   let moves = [];
   let prunedMovesList = [];
-  if (pieceType === cnst.WHITE_KING){
+  if (pieceType === cnst.WHITE_KING) {
     debugger;
   }
 
@@ -239,34 +296,35 @@ function getKingMoves(i, y, squares, pieceType, hasNotCastled) {
     { x: i + 1, y: y - 1 },
     { x: i, y: y - 1 },
     { x: i - 1, y: y - 1 },
-    { x: i - 1, y: y }
+    { x: i - 1, y: y },
   ];
 
   //prune list for moves that go off the board and cause errors. We'll have to do this because unlike things like getBishopMoves etc. the king has hardcoded moves with no checks/restraints
-  potentialMovesList.forEach(potentialMove => {
-    if (potentialMove.x < cnst.MIN_X || potentialMove.x > cnst.MAX_X || potentialMove.y < cnst.MIN_Y || potentialMove.y > cnst.MAX_Y) {
+  potentialMovesList.forEach((potentialMove) => {
+    if (
+      potentialMove.x < cnst.MIN_X ||
+      potentialMove.x > cnst.MAX_X ||
+      potentialMove.y < cnst.MIN_Y ||
+      potentialMove.y > cnst.MAX_Y
+    ) {
       //do nothing because it is outside board constraints
-    }
-    else {
+    } else {
       //otherwise its within restraints, so add it pruned list
       prunedMovesList.push(potentialMove);
     }
   });
 
-  prunedMovesList.forEach(prunedMove => {
+  prunedMovesList.forEach((prunedMove) => {
     if (pieceType === cnst.WHITE_KING) {
     }
     if (isBlocking(prunedMove.x, prunedMove.y, squares)) {
       //if there is a block, check if oppenent or own piece. if oppon, add the move and break loop
       if (isBlockingOpp(prunedMove.x, prunedMove.y, squares, pieceType)) {
         moves.push({ x: prunedMove.x, y: prunedMove.y });
-      }
-      else {
+      } else {
         // otherwise dont add to moves list
       }
-    }
-
-    else {
+    } else {
       moves.push({ x: prunedMove.x, y: prunedMove.y });
     }
   });
@@ -287,34 +345,33 @@ function getBishopMoves(i, y, squares, pieceType) {
   //upward right diagonal
   tempi = i;
   tempy = y;
-  for (q = y; q <= cnst.MAX_Y - 1; q++ ) {
-      tempi = tempi - 1;
-      tempy = tempy + 1;
-      
-      if (tempi < cnst.MIN_Y || tempi > cnst.MAX_X || tempy < cnst.MIN_Y || tempy > cnst.MAX_Y){
+  for (q = y; q <= cnst.MAX_Y - 1; q++) {
+    tempi = tempi - 1;
+    tempy = tempy + 1;
+
+    if (
+      tempi < cnst.MIN_Y ||
+      tempi > cnst.MAX_X ||
+      tempy < cnst.MIN_Y ||
+      tempy > cnst.MAX_Y
+    ) {
+      break;
+    }
+
+    //first check if a piece is blocking
+    if (isBlocking(tempi, tempy, squares)) {
+      //if there is a block, check if oppenent or own piece. if oppon, add the move and break loop
+      if (isBlockingOpp(tempi, tempy, squares, pieceType)) {
+        moves.push({ x: tempi, y: tempy });
         break;
-    }
-
-  //first check if a piece is blocking
-  if (isBlocking(tempi, tempy, squares)){
-
-    //if there is a block, check if oppenent or own piece. if oppon, add the move and break loop
-    if (isBlockingOpp(tempi, tempy, squares, pieceType)){
+      } else {
+        // otherwise dont add move and break loop
+        break;
+      }
+    } else {
       moves.push({ x: tempi, y: tempy });
-      break;
-    }
-
-    else {
-      // otherwise dont add move and break loop
-      break;
     }
   }
-
-  else {
-    moves.push({ x: tempi, y: tempy });
-  }
-
-}
 
   //upward left diagonal
   tempi = i;
@@ -323,29 +380,28 @@ function getBishopMoves(i, y, squares, pieceType) {
     tempi = tempi - 1;
     tempy = tempy - 1;
 
-    if (tempi < cnst.MIN_Y || tempi > cnst.MAX_X || tempy < cnst.MIN_Y || tempy > cnst.MAX_Y) {
+    if (
+      tempi < cnst.MIN_Y ||
+      tempi > cnst.MAX_X ||
+      tempy < cnst.MIN_Y ||
+      tempy > cnst.MAX_Y
+    ) {
       break;
     }
 
     //first check if a piece is blocking
     if (isBlocking(tempi, tempy, squares)) {
-
       //if there is a block, check if oppenent or own piece. if oppon, add the move and break loop
       if (isBlockingOpp(tempi, tempy, squares, pieceType)) {
         moves.push({ x: tempi, y: tempy });
         break;
-      }
-
-      else {
+      } else {
         // otherwise dont add move and break loop
         break;
       }
-    }
-
-    else {
+    } else {
       moves.push({ x: tempi, y: tempy });
     }
-
   }
 
   //downward right diagonal
@@ -355,29 +411,28 @@ function getBishopMoves(i, y, squares, pieceType) {
     tempi = tempi + 1;
     tempy = tempy + 1;
 
-    if (tempi < cnst.MIN_Y || tempi > cnst.MAX_X || tempy < cnst.MIN_Y || tempy > cnst.MAX_Y) {
+    if (
+      tempi < cnst.MIN_Y ||
+      tempi > cnst.MAX_X ||
+      tempy < cnst.MIN_Y ||
+      tempy > cnst.MAX_Y
+    ) {
       break;
     }
 
     //first check if a piece is blocking
     if (isBlocking(tempi, tempy, squares)) {
-
       //if there is a block, check if oppenent or own piece. if oppon, add the move and break loop
       if (isBlockingOpp(tempi, tempy, squares, pieceType)) {
         moves.push({ x: tempi, y: tempy });
         break;
-      }
-
-      else {
+      } else {
         // otherwise dont add move and break loop
         break;
       }
-    }
-
-    else {
+    } else {
       moves.push({ x: tempi, y: tempy });
     }
-
   }
   //downward left diagonal
   tempi = i;
@@ -386,29 +441,28 @@ function getBishopMoves(i, y, squares, pieceType) {
     tempi = tempi + 1;
     tempy = tempy - 1;
 
-    if (tempi < cnst.MIN_Y || tempi > cnst.MAX_X || tempy < cnst.MIN_Y || tempy > cnst.MAX_Y) {
+    if (
+      tempi < cnst.MIN_Y ||
+      tempi > cnst.MAX_X ||
+      tempy < cnst.MIN_Y ||
+      tempy > cnst.MAX_Y
+    ) {
       break;
     }
 
     //first check if a piece is blocking
     if (isBlocking(tempi, tempy, squares)) {
-
       //if there is a block, check if oppenent or own piece. if oppon, add the move and break loop
       if (isBlockingOpp(tempi, tempy, squares, pieceType)) {
         moves.push({ x: tempi, y: tempy });
         break;
-      }
-
-      else {
+      } else {
         // otherwise dont add move and break loop
         break;
       }
-    }
-
-    else {
+    } else {
       moves.push({ x: tempi, y: tempy });
     }
-
   }
 
   return moves;
@@ -420,63 +474,51 @@ function getRookMoves(i, y, squares, pieceType) {
 
   //downward movement
   for (p = i + 1; p <= cnst.MAX_X; p++) {
-
-  //first check if a piece is blocking
-  if (isBlocking(p, y, squares)){
-
+    //first check if a piece is blocking
+    if (isBlocking(p, y, squares)) {
       //if there is a block, check if oppenent or own piece. if oppon, add the move and break loop
-      if (isBlockingOpp(p, y, squares, pieceType)){
+      if (isBlockingOpp(p, y, squares, pieceType)) {
         moves.push({ x: p, y: y });
         break;
-      }
-      else {
+      } else {
         // otherwise dont add move and break loop
         break;
       }
-    }
-  else {
-    moves.push({ x: p, y: y });
+    } else {
+      moves.push({ x: p, y: y });
     }
   }
 
   //upward movement
   for (p = i - 1; p >= cnst.MIN_X; p--) {
-
     //first check if a piece is blocking
     if (isBlocking(p, y, squares)) {
-
       //if there is a block, check if oppenent or own piece. if oppon, add the move and break loop
       if (isBlockingOpp(p, y, squares, pieceType)) {
         moves.push({ x: p, y: y });
         break;
-      }
-      else {
+      } else {
         // otherwise dont add move and break loop
         break;
       }
-    }
-    else {
+    } else {
       moves.push({ x: p, y: y });
     }
   }
 
   // movement to the right
   for (p = y + 1; p <= cnst.MAX_Y; p++) {
-
     //first check if a piece is blocking
     if (isBlocking(i, p, squares)) {
-
       //if there is a block, check if oppenent or own piece. if oppon, add the move and break loop
       if (isBlockingOpp(i, p, squares, pieceType)) {
         moves.push({ x: i, y: p });
         break;
-      }
-      else {
+      } else {
         // otherwise dont add move and break loop
         break;
       }
-    }
-    else {
+    } else {
       moves.push({ x: i, y: p });
     }
   }
@@ -485,18 +527,15 @@ function getRookMoves(i, y, squares, pieceType) {
   for (p = y - 1; p >= cnst.MIN_Y; p--) {
     //first check if a piece is blocking
     if (isBlocking(i, p, squares)) {
-
       //if there is a block, check if oppenent or own piece. if oppon, add the move and break loop
       if (isBlockingOpp(i, p, squares, pieceType)) {
         moves.push({ x: i, y: p });
         break;
-      }
-      else {
+      } else {
         // otherwise dont add move and break loop
         break;
       }
-    }
-    else {
+    } else {
       moves.push({ x: i, y: p });
     }
   }
@@ -508,22 +547,57 @@ function getQueenMoves(i, y, squares, pieceType) {
   let moves = [];
 
   let rookMoves = getRookMoves(i, y, squares, pieceType);
-  let bishMoves = getBishopMoves(i ,y, squares, pieceType);
+  let bishMoves = getBishopMoves(i, y, squares, pieceType);
   moves = rookMoves.concat(bishMoves);
 
   return moves;
 }
 
+/**
+ * @description     This function gets all the moves for a pawn. Note the relative directions for the pawn moves are hardcoded.
+ *                  TODO: add diagonal moves for capturing a piece with the pawn.
+ *
+ */
 function getPawnMoves(i, y, c, isFirstMove) {
   let moves = [];
 
   if (c === cnst.WHITE_PAWN) {
     moves.push({ x: i - 1, y: y });
-    if (isFirstMove) moves.push({ x: i - 2, y: y });
+
+    if (isFirstMove) {
+      moves.push({ x: i - 2, y: y });
+    }
   } else if (c === cnst.BLACK_PAWN) {
-    moves.push({ x: i, y: y + 1 });
-    if (isFirstMove) moves.push({ x: i, y: y + 2 });
+    moves.push({ x: i + 1, y: y });
+
+    if (isFirstMove) {
+      moves.push({ x: i + 2, y: y });
+    }
   }
 
   return moves;
+}
+
+/**
+ *  @description    This function returns true if the pawn has not moved yet, and false if it has already moved.
+ *                  This has been hard coded assuming that the white pieces are on the bottom of the board (row 6).
+ *                  TODO: If we implement variable board configuration (the player can play as either black or white, we need to check for the appropriate row.)
+ *                  For now we assume that white pawns must be on row 6, and that black pawns must be on row 1 for them not to have moved.
+ */
+function isFirstPawnMove(pawnColor, x) {
+  if (pawnColor === cnst.WHITE_PAWN) {
+    if (x !== 6) {
+      return false;
+    } else {
+      return true;
+    }
+  } else if (pawnColor === cnst.BLACK_PAWN) {
+    if (x !== 1) {
+      return false;
+    } else {
+      return true;
+    }
+  } else {
+    throw new Error(`Not a pawn being selected for isFirstPawnMove`);
+  }
 }
